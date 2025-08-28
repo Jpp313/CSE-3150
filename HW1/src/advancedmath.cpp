937723c2e79a1b17ad76be1719980b96fb6419f8
JP <jp@JP.localdomain>
2025-08-28 16:23:42 -0400

    HW1

---
+++ HW1/src/advancedmath.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "mathutils.h"
+#include "advancedmath.h"
+
+namespace AdvancedMath{
+
+int square(int n){
+    return MathUtils::multiply(n,n);
+}
+
+}
---
+++ HW1/src/mathutils.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "mathutils.h"
+
+namespace MathUtils {
+
+int add(int a, int b){
+    return a + b;
+}
+
+int multiply(int a , int b){
+    return a * b;
+}
+
+}
