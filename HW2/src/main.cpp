5ece0c8c689691f2fbc5aabc20cd3962c87093dd
JP <jp@JP.localdomain>
2025-09-08 13:42:45 -0400

    HW2

--- HW2/src/main.cpp
+++ HW2/src/main.cpp
@@ -6,7 +6,7 @@
 using std::cout,std::cin,std::string, std::endl;
 
 int main(){
-	//cout << "Enter name: ";
+	//cout << "Enter name: "; test.py file does not want input to have Enter Name
 	string name;
 	std::getline(cin, name);
 	string greeting = Greeting_Utils::create_message(name);
